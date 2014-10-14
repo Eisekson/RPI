4f314fe7609f93b1770a821e71ab3f511ba88b7a
eisekson <ksa541@gmail.com>
2014-10-14 16:31:38 +0800

    f

--- ser.py
+++ ser.py
@@ -3,7 +3,7 @@ import sys
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('localhost', 10000)
+server_address = ('192.168.1.103', 10000)
 print >> sys.stderr, 'starting up on %s port %s' % server_address
 sock.bind(server_address)
 sock.listen(1)
